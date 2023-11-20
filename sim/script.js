7703446701703e500b49ed91281ffa64fd86c679
Vikas Kumar <138956971+webdev12023@users.noreply.github.com>
2023-11-20 19:05:48 +0530

    Delete sim directory

--- sim/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const nightModeToggle = document.getElementById('nightModeToggle');
-    const body = document.body;
-
-    nightModeToggle.addEventListener('click', function () {
-        body.classList.toggle('night-mode');
-    });
-});
